8d369abd9dfbc37c33d1830bab48a13cb2c50eca
Fernando Molano <fm87usa@gmail.com>
2021-09-08 18:33:27 -0500

    add option to play multiple times without re-running the program

--- guess_the_number.py
+++ guess_the_number.py
@@ -39,20 +39,23 @@ def check_guess(guess, secret, counter):
 
 
 def main():
+    play = input("Press 'Y' to play or Enter to quit ")
 
-    (low, high) = configure_range()
-    secret = generate_secret(low, high)
+    while play.lower() == 'y':
+        (low, high) = configure_range()
+        secret = generate_secret(low, high)
 
-    number_of_guesses = 1  # count how many times user tried to guess
-    while True:
-        guess = get_guess()
-        (result, number_of_guesses) = check_guess(
-            guess, secret, number_of_guesses)
-        print(result)
+        number_of_guesses = 1  # count how many times user tried to guess
+        while True:
+            guess = get_guess()
+            (result, number_of_guesses) = check_guess(
+                guess, secret, number_of_guesses)
+            print(result)
 
-        if result == correct:
-            print(f'It took you {number_of_guesses} tries to guess it')
-            break
+            if result == correct:
+                print(f'It took you {number_of_guesses} tries to guess it')
+                break
+        play = input("Press 'Y' to play again or Enter to quit ")
 
     print('Thanks for playing the game!')
 
